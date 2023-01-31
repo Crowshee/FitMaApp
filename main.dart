81a14cfcc6ebb33dc2fd0847d3d8c7fcff4089ec
Crowe <121046946+Crowshee@users.noreply.github.com>
2023-01-31 20:03:40 +0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:fitt_app/LogInPage.dart';
 import 'package:flutter/material.dart';
 
-
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -10,67 +14,50 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(backgroundColor: Color(0xff95E1E9)),
-      home: FirstScreen(
-      ),
+      home: FirstScreen(),
     );
   }
 }
 
 class FirstScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Container(
-
       decoration: const BoxDecoration(
-        gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [Color(0xff0D3728),Color(0xff3D5E52)])),
-
-    child: Scaffold(
-      backgroundColor: Colors.transparent,
-
-      body: Center(
-        child: Column(
-          children: <Widget>[
-
-            Container(// LogIn Button
-              margin: EdgeInsets.only(top: 550.0, bottom: 5.0),
-              height: 50,
-              width: 350,
-              decoration: BoxDecoration(
-                  color: Color(0xff95E1E9).withOpacity(0.2), borderRadius: BorderRadius.circular(10)),
-              child: TextButton(
-                onPressed: (){
-                  Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => LogInPage()),
-                  );
-                },
-                child: Text(
-                  'Get Started',
-                  style: TextStyle(color: Colors.white, fontSize: 25),
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Color(0xff11b4bb), Color(0xffa7f5f8)])),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                // LogIn Button
+                margin: EdgeInsets.only(top: 600.0, bottom: 5.0),
+                height: 50,
+                width: 350,
+                decoration: BoxDecoration(
+                    color: Color(0xff11b4bb).withOpacity(0.4),
+                    borderRadius: BorderRadius.circular(10)),
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LogInPage()),
+                    );
+                  },
+                  child: Text(
+                    'Get Started',
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
                 ),
               ),
-            ),
-
-          ],
+            ],
+          ),
         ),
       ),
-
-    ),
-
     );
-
-
   }
 }
-
-
-
-
-
-
-
-
-
